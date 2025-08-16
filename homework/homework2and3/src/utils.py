f5723d22095bde038140102cffa6388de59fd9a4
Shreya Volety <svolety@10-17-125-52.dynapool.wireless.nyu.edu>
2025-08-16 18:58:33 -0400

    Adding try/except function and numpy operation to utils.py

--- homework/homework2and3/src/utils.py
+++ homework/homework2and3/src/utils.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import numpy as np
 
 def get_summary_stats(df):
     print(df.info())
     print(df[['mpg','displacement','horsepower','acceleration']].describe())
     
+def small_op(lst):
+    print (f'''List average: {np.average(lst)}''')
+    print (f'''List sum: {np.sum(lst)}''')
+    print (f'''List standard dev: {np.std(lst)}''')
